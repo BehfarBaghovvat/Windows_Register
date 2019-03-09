87bd8210f49bde79ad1972bd9af5d1a0c025da91
Behfar Baghovvat <behfar.baghovvat@gmail.com>
2019-03-10 01:51:17 +0330

    Making some changes...!

--- Windows_Register/Models/User.cs
+++ Windows_Register/Models/User.cs
@@ -1,37 +1,73 @@
 ﻿namespace Models
 {
-    /// <summary>
-    /// User calss.
-    /// </summary>
-     public class User: object
-    {
-        /// <summary>
-        /// Costrucor user.
-        /// </summary>
-        public User():base()
-        {
+	/// <summary>
+	/// User calss.
+	/// </summary>
+	public class User : object
+	{
+		/// <summary>
+		/// Costrucor user.
+		/// </summary>
+		public User() : base()
+		{
 
-        }
+		}
 
-        /// <summary>
-        /// Firs username.
-        /// </summary>
-        public string FirstName { get; set; }
+		/// <summary>
+		/// FirstName username.
+		/// </summary>
+		public string FirstName { get; set; }
 
-        /// <summary>
-        /// Last user name.
-        /// </summary>
-        public string LastName { get; set; }
+		/// <summary>
+		/// LastName username.
+		/// </summary>
+		public string LastName { get; set; }
 
-        /// <summary>
-        /// Gender user.
-        /// </summary>
-        public Models.Enums.Gender Gender { get; set; }       
 
-        /// <summary>
-        /// Security questions can be used to access the account.
-        /// </summary>
-        public Models.Enums.Question SecutrityQuestion { get; set; }
-        
-    }
+		/// <summary>
+		/// Birth username
+		/// </summary>
+		public System.DateTime BirthDate { get; set; }
+
+		/// <summary>
+		/// Password username.
+		/// </summary>
+		public string Password { get; set; }
+
+		/// <summary>
+		/// E-mail username.
+		/// </summary>
+		public string Email { get; set; }
+
+		/// <summary>
+		/// Gender username.
+		/// </summary>
+		public Models.Enums.Gender Gender { get; set; }
+
+		/// <summary>
+		/// Security questions can be used to access the account.
+		/// </summary>
+		public Models.Enums.Question SecutrityQuestion { get; set; }
+
+		/// <summary>
+		/// Which pages have visited
+		/// </summary>
+		public string GetDirection { get; set; }
+
+		/// <summary>
+		/// What files have uploaded
+		/// </summary>
+		public string GetUploadFile { get; set; }
+
+		/// <summary>
+		/// What files to download
+		/// </summary>
+		public string GetDownloadFile { get; set; }
+
+		/// <summary>
+		/// With what IP has been communicated.
+		/// </summary>
+		public int CommunicationIP { get; set; }
+
+	}
 }
